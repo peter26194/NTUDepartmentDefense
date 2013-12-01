8c713e0b5eeb44582a5a8496eebd9ebb6ee17d2a
peter26194 <peter26194@gmail.com>
2013-12-01 12:41:39 +0800

    new Class Position

---
+++ src/com/example/ntudepartmentdefense/util/Position.java
@@ -0,0 +1,10 @@
+package com.example.ntudepartmentdefense.util;
+
+public class Position {
+	public short[] x;
+	public short[] y;
+	public Position(short[] x, short[] y){
+		this.x = x;
+		this.y = y;
+	}
+}
